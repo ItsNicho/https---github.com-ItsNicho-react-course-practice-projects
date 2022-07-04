662ee3fcc83be7b91d64b4f605329cd15f512b7c
Nicolas Camous <ncamous@gmail.com>
2022-07-04 12:37:16 -0300

    practice-projects

---
+++ 01-practice-project/src/components/Expenses/Expenses.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
+import ExpensesChart from "./ExpensesChart";
+import "./Expenses.css";
+import ExpensesChart from "./ExpensesChart";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("Show All");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    if (filteredYear !== "Show All") {
+      return expense.date.getFullYear().toString() === filteredYear;
+    } else {
+      return true;
+    }
+  });
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onFilterChange={filterChangeHandler}
+        />
+        <ExpensesChart expense={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
